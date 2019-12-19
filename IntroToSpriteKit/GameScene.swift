ca9507c35bb335e5dd4cb9cef504c90330255d4a
Dima Dovgalyk <ddovgalyk@lcs.on.ca>
2019-12-18 22:18:23 -0500

    added a little santa that does until the middle of the screen

--- IntroToSpriteKit/GameScene.swift
+++ IntroToSpriteKit/GameScene.swift
@@ -48,7 +48,7 @@ class GameScene: SKScene {
             snowflake.physicsBody = SKPhysicsBody(circleOfRadius: snowflake.size.width * 0.5)
             //adds the snowflake to the scene
             self.addChild(snowflake)
-            //removes the snowflake after a second 
+            //removes the snowflake after a second
             snowflake.run(SKAction.sequence([SKAction.wait(forDuration: 1.0),SKAction.removeFromParent()]))
             
         }
@@ -59,15 +59,44 @@ class GameScene: SKScene {
         //creates a sequence, first add then wait
         let sequenceSpawnThenWait = SKAction.sequence([actionSpawnSnowFlake, actionWait])
         //repeats the sequence 1000 times
-        let actionRepeatlyAddSnowFlake = SKAction.repeat(sequenceSpawnThenWait, count: 1000)
+        let actionRepeatlyAddSnowFlake = SKAction.repeat(sequenceSpawnThenWait, count: 300)
         //adds snowflakes to the scene
         self.run(actionRepeatlyAddSnowFlake)
+        self.run(actionRepeatlyAddSnowFlake)
+        self.run(actionRepeatlyAddSnowFlake)
+
+        let hero = SKSpriteNode(imageNamed: "AOCXmasGifts_01")
+        hero.position = CGPoint(x: hero.size.width / 2, y: hero.size.height / 2)
+        hero.physicsBody?.isDynamic = false
+        self.addChild(hero)
+        // Create an empty array of SKTexture objects
+        var walkingTextures: [SKTexture] = []
+
+        // Now add the two images we need in the array
+        walkingTextures.append(SKTexture(imageNamed: "AOCXmasGifts_01"))
+        walkingTextures.append(SKTexture(imageNamed: "AOCXmasGifts_02"))
+        //walkingTextures.append(SKTexture(imageNamed: "AOCXmasGifts_03"))
+     
         
-//        let actionWaitTenSec = SKAction.wait(forDuration: 10)
-//        let actionRemoveAllSnowFlakes = SKAction.removeFromParent()
-//        let sequenceDespawn = SKAction.sequence([actionWaitTenSec, actionRemoveAllSnowFlakes])
-//        self.run(sequenceDespawn)
-//
+        // Create an action to animate a walking motion using the hero sprites array (walkingTextures)
+        let actionWalkingAnimation = SKAction.animate(with: walkingTextures, timePerFrame: 0.2, resize: true, restore: true)
+
+        // Create an action that moves the hero forward a "step" where a step is 10 pixels
+        // NOTE: The time interval for moving forward matches the time per frame of the animation
+        let actionMoveForward = SKAction.moveBy(x: 10, y: 0, duration: 0.2)
+
+        // Repeat the move forward action twice
+        let actionMoveForwardTwice = SKAction.repeat(actionMoveForward, count: 2)
+
+        // Now, combine the walking animation with the sprite moving forward
+        let actionWalkAndMove = SKAction.group([actionWalkingAnimation, actionMoveForwardTwice])
+
+        // Repeat the "walk and move" action five times
+        let actionWalkAndMoveFiveTimes = SKAction.repeat(actionWalkAndMove, count: 20)
+
+        // Make the hero walk and move forward five times
+        hero.run(actionWalkAndMoveFiveTimes)
+
         
         
     }
